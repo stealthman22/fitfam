b16f8c523a781d3138f965e45e8d2806e5acd385
stealthman22 <tdotonline@gmail.com>
2022-10-19 21:52:13 +0000

    working on the reasons section

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
 	return (
@@ -8,6 +10,21 @@ const Programs = () => {
 				<span className="stroke-text">Check out our</span>
 				<span>Programs</span>
 				<span className="stroke-text">that will get you into shape</span>
+			</div>
+			<div className="program-categories">
+				{programsData.map((program) => (
+					<div className="category">
+						{program.image}
+						<span>{program.heading}</span>
+						<span>{program.details}</span>
+						<div className="join-now">
+							<span>Join Now</span>
+							<span>
+								<img src={RightArrow} alt="" />
+							</span>
+						</div>
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -17,7 +17,37 @@ const Reasons = () => {
 				<img src={image2} alt="" />
 				<img src={image3} alt="" />
 				<img src={image4} alt="" />
-				<right className="r">right-side</right>
+			</div>
+			<div className="right-r">
+				<span>Few reasons</span>
+				<div>
+					<span className="stroke-text">Why </span>
+					<span>choose us ?</span>
+				</div>
+				<div className="details-r">
+					<div>
+						<img src={tick} alt="" />
+						<span> over 20 expert coaches</span>
+					</div>
+					<div>
+						<img src={tick} alt="" />
+						<span> have a community of like minded friends</span>
+					</div>
+					<div>
+						<img src={tick} alt="" />
+						<span> 1 free program for every member</span>
+					</div>
+					<div>
+						<img src={tick} alt="" />
+						<span> Reliable partners</span>
+					</div>
+				</div>
+				<span className="partners-heading">Our Partners</span>
+				<div className="partners">
+					<img src={nb} alt="" />
+					<img src={adidas} alt="" />
+					<img src={nike} alt="" />
+				</div>
 			</div>
 		</div>
 	);
